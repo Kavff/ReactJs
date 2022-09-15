0ed46beb5cc2c0048ce4b85a107f5e05a14ab448
Lucas <lucas.lobos12@gmail.com>
2022-09-14 21:40:39 -0400

    problemas con firebase

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,36 +1,36 @@
 import { useEffect, useState } from "react";
 import "./ItemsListContainer.scss";
 import ItemList from "../ItemList/ItemList";
-import dataRequest from "../../helpers/dataRequest";
 import ContainerTitle from "../ContainerTitle/ContainerTitle";
 import { useParams } from "react-router-dom";
 import Loader from "../Loader/Loader";
-
+import { collection,getDocs } from "firebase/firestore"
+import { db } from '../../firebase/config'
 
 const ItemListContainer = () => {
   // Here in the container i use THE logic for my request from my API.
-
+ 
   const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true); 
   const { categoryId } = useParams();
 
   useEffect(() => {
-    dataRequest()
-      .then((res) => {
-        if (!categoryId) {
-          setProducts(res);
-        } else {
-          setProducts(res.filter((prod) => prod.category === categoryId));
-        }
-        setLoading(true);
+    
+    //1-  Built reference
+      const productosRef = collection(db, 'productos');
+    getDocs(productosRef)
+      .then((resp) => {
+        const productosDB = resp.docs.map  ((doc) => ({id : doc.id, ...doc.data()}) )
+        console.log(productosDB)
+        setProducts(productosDB)
+
+      }).finally(() => {
+        setLoading(false)
       })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+
+    //2-  Consume reference
   }, [categoryId]);
+
 
   return (
     <>
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBC5-BnPcxZ1SUsf8mlw1ZbJ7S62p-e_IA",
+  authDomain: "rj-coderll.firebaseapp.com",
+  projectId: "rj-coderll",
+  storageBucket: "rj-coderll.appspot.com",
+  messagingSenderId: "806729165584",
+  appId: "1:806729165584:web:5a2d7070be082fbeb95395"
+};
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
