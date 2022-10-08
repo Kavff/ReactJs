9bfabf63c5635a70d1275e4fc85a880d2738eebf
Lucas <lucas.lobos12@gmail.com>
2022-10-08 19:44:12 -0400

    test7

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -2,7 +2,7 @@ import IconButton from "@mui/material/IconButton";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import "./CartWidget.scss";
 import { Link } from "react-router-dom";
-import React, {  useContext }/*,  { useContext } */ from "react";
+import React, {  useContext } from "react";
  import { CartContext }  from "../../Context/CartContext";
 
 
