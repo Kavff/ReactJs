84912a2525315535aed864949bbe68995d343c7b
Lucas Lobos <98063036+LucasLobos@users.noreply.github.com>
2022-10-10 14:50:39 -0400

    Update itemCart.js

--- src/components/itemCart/itemCart.js
+++ src/components/itemCart/itemCart.js
@@ -2,7 +2,7 @@ import { IconButton } from "@mui/material";
 import React, { useContext } from "react";
 import "./ItemCart.scss";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { CartContext } from "../../context/CartContext";
+import { CartContext } from "../../context/CartContext1";
 
 const ItemCart = ({ product,handleAddToCart}) => {
 
