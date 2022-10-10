c8a2a89cc167993df92edc9fc6224706df66c3a4
Lucas Lobos <98063036+LucasLobos@users.noreply.github.com>
2022-10-10 14:46:46 -0400

    Update CartWidget.js

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -3,7 +3,7 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import "./CartWidget.scss";
 import { Link } from "react-router-dom";
 import React, {  useContext } from "react";
- import { CartContext }  from "../../context/CartContext";
+ import { CartContext }  from "../../context/CartContext1";
 
 
 export function CartWidget() {
